3982d37c0e319e93b5356237fbdd9850d83c6c37
Case Larsen <clarsen@gmail.com>
2018-02-08 20:42:45 -0800

    fix update_peloton to use SheetAdapter

--- examples/update_peloton.py
+++ examples/update_peloton.py
@@ -17,58 +17,16 @@ import sync_google_spreadsheet.sheet_adapter
 
 
 class WorkoutSheet(sync_google_spreadsheet.sheet_adapter.SheetAdapter):
-    def __init__(self, sheet, cellrange, rows, columns,
-                 column_for_key, column_for_value):
-        self.columns = columns
-        self.rows = rows
-        self.range = cellrange
-        self.column_name_to_column = {}
-        self.next_empty_row = None
+    def __init__(self, sheet_adapter):
 
-        self.value_cell_for_key = {}
-        self.column_for_key = column_for_key
-        self.column_for_value = column_for_value
-        super(WorkoutSheet, self).__init__(sheet)
-
-    def load(self):
-        self.cell_list = self.sheet.range(self.range)
-
-        for column in range(self.columns):
-            self.column_name_to_column[self.cell_at(0, column).value] = column
-
-        for row in range(1, self.rows):
-            # empty?
-            if self.cell_at(row, self.column_for_key).value == '':
-                self.next_empty_row = row
-                break
-
-            key = pd.Timestamp(self.cell_at(row, self.column_for_key).value,
+        def rowkey(row):
+            key = pd.Timestamp(row['Workout Timestamp (PST)'],
                                tz='America/Los_Angeles')
-            self.value_cell_for_key[key] \
-                = self.cell_at(row, self.column_for_value)
+            return key
 
-    def cell_at(self, row, col):
-        return self.cell_list[self.columns * row + col]
-
-    def exists(self, key):
-        return key in self.value_cell_for_key
-
-    def value(self, key):
-        return self.value_cell_for_key[key].value
-
-    def update(self, key, value):
-        self.value_cell_for_key[key].value = value
-
-    def append(self, row_dict):
-        print("would add to row {}".format(self.next_empty_row))
-        row = self.next_empty_row
-        for key in row_dict.keys():
-            col = self.column_name_to_column[key]
-            self.cell_at(row, col).value = row_dict[key]
-        self.next_empty_row += 1
-
-    def sync(self):
-        self.sheet.update_cells(self.cell_list)
+        super(WorkoutSheet, self).__init__(sheet_adapter, 1,
+                                           rowkey,
+                                           non_empty_column='Workout Timestamp (PST)')
 
 
 def update_peloton(secrets):
@@ -86,9 +44,10 @@ def update_peloton(secrets):
     # with headless chrome, doesn't seem to be able to trigger the download
     if False:
         chrome_options.add_argument('--headless')
-        chrome_options.add_argument("--window-size=1920x1080")
+        chrome_options.add_argument("--window-size=1200x1080")
         # Replaces browser User Agent from "HeadlessChrome".
-        user_agent = "Chrome"
+        user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36"
+
         chrome_options.add_argument('--user-agent="{user_agent}"'
                                     .format(user_agent=user_agent))
     chrome_prefs = {
@@ -135,22 +94,22 @@ def update_peloton(secrets):
     ActionChains(browser).move_to_element(button).click().perform()
 
 
-def update_peloton2(secrets, sheet):
-    pw = WorkoutSheet(sheet,
-                      secrets['sheet']['range'],
-                      secrets['sheet']['rows'],
-                      secrets['sheet']['columns'],
-                      0, 1)
-    pw.load()
-    files = glob.glob("./tmp/*.csv")
-    with open(files[0], "r") as f:
+def csv_streamer(fn):
+    with open(fn, "r") as f:
         reader = csv.DictReader(f)
         for row in reader:
-            ts_val = row['Workout Timestamp (PST)']
-            ts = pd.Timestamp(ts_val, tz="America/Los_Angeles")
-            print(ts, row['Total Output'])
-            if not pw.exists(ts):
-                pw.append(row)
+            yield row
+
+
+def update_peloton2(secrets, sheet):
+    pw = WorkoutSheet(sheet)
+    pw.load()
+
+    files = glob.glob("./tmp/*.csv")
+    for row in csv_streamer(files[0]):
+        print(row['Workout Timestamp (PST)'], row['Total Output'])
+        if not pw.has(row):
+            pw.append(row)
     pw.sync()
 
 
@@ -167,7 +126,6 @@ def test():
 def init_common():
     with open(os.path.expanduser("~/.secrets-peloton.yaml"), "r") as f:
         secrets = yaml.load(f)
-    print(secrets)
     SCOPE = ["https://spreadsheets.google.com/feeds"]
     SECRETS_FILE = os.path.expanduser(secrets['sheet']['secrets_file'])
     SPREADSHEET = secrets['sheet']['name']
